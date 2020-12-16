1dc0d29cd3a6c3fcfb1425ddaf4398fe455d2d24
Kindered <pro.thomaspoulard@gmail.com>
2020-12-16 22:45:12 +0100

    Final commit

--- se1c_test/src/App.js
+++ se1c_test/src/App.js
@@ -5,41 +5,52 @@ import SubmitInput from "./components/SubmitInput/SubmitInput";
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSubmitted : false,
+      mail : null,
+    };
+  }
+
+  onSubmit(e) {
+    const form = new FormData(e.target);
+    const mail = form.get("mail");
+    e.preventDefault();
+    this.setState({isSubmitted : true, mail : mail})    
+  }
+
   render() {
+
     return (
       <div id="wrapper">
         <img
           src="https://syndic-en-un-clic.fr/static/syndic-en-un-clic-logo_white-007b901bf15d8b38c3e0f27d8921de31.svg"
           alt="syndic en un clic"
         />
+          {this.state.isSubmitted
+                ? <p>Bienvenue : {this.state.mail} </p>
+                : <div id="formWrapper">
 
-        <div id="formWrapper">
-          <form id="form">
-            <LabelledInput
-              type="email"
-              placeholder="votre@mail.fr"
-              id="mail"
-              name="mail"
-            />
+                <form id="form" onSubmit={(e) =>  this.onSubmit(e)  }>
+                      
+                  <LabelledInput type="email" placeholder="votre@mail.fr" id="mail" name="mail"/>
+      
+                  <LabelledInput type="password" placeholder="*********" id="pwd" name="pwd"/>
+      
+                  <SubmitInput type="submit" value="SE CONNECTER" id="submit"/>
+      
+                </form>
+              </div>
+          }
 
-            <LabelledInput
-              type="password"
-              placeholder="*********"
-              id="pwd"
-              name="pwd"
-            />
-
-            <SubmitInput
-            type="submit" 
-            value="SE CONNECTER" 
-            id="submit"
-            />
-
-          </form>
-        </div>
       </div>
     );
   }
+
+
+
 }
 
 export default App;
